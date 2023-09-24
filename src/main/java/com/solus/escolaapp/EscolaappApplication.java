c344d0ecff629a9b1afadfdff3c01f2b23088c87
Albert Thomaz <thzalbert@gmail.com>
2023-09-24 09:41:55 -0300

    feat: add estrutura do projeto

---
+++ src/main/java/com/solus/escolaapp/EscolaappApplication.java
@@ -0,0 +1,13 @@
+package com.solus.escolaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solus/escolaapp/EscolaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solus.escolaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
